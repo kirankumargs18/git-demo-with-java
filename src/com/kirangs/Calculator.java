cae75448234377c0ca25f97e0df71f4428d437af
kirankumargs18 <kirangskumar98@gmail.com>
2024-10-25 01:00:31 +0530

    modified TestMain and added Calculator

---
+++ src/com/kirangs/Calculator.java
@@ -0,0 +1,9 @@
+package com.kirangs;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+
+	}
+
+}
--- src/com/kirangs/TestMain.java
+++ src/com/kirangs/TestMain.java
@@ -4,11 +4,18 @@ public class TestMain {
 
 	public static void main(String[] args) {
 
+		TestMain testMain = new TestMain();
+		System.out.println(testMain.getMessageInstance());
+
 		System.out.println("Objective : " + getMessage());
 	}
 
 	private static String getMessage() {
 
 		return "Lets explore GIT";
+	}
+
+	private String getMessageInstance() {
+		return "Instnace method";
 	}
 }
